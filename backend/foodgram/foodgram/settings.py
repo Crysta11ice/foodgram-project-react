6cc459facfb3fff0f286e19a35bbf1dcecc20441
V <crystallice@yandex.ru>
2022-09-11 17:46:52 +0300

    ver 1.67 not fixed; checking settings change

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -121,9 +121,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
     "DEFAULT_PERMISSION_CLASSES": [
